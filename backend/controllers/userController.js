652ed49d8d5a442cea0493efadd8b2846cd0b943
who am i <62942231+sachira-madhushan@users.noreply.github.com>
2024-05-22 12:48:06 +0530

    User Registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,30 @@
-export const registerUser=(req,res)=>{
-    res.send("Register");
+import connectDB from '../configs/db.js';
+import bcrypt from 'bcryptjs'
+
+const db=connectDB();
+
+export const registerUser=async(req,res)=>{
+
+    const salt=await bcrypt.genSalt(10);
+    const hashedPass=await bcrypt.hash(req.body.password,salt);
+
+    const values=[
+        req.body.name,
+        req.body.email,
+        hashedPass
+    ]
+    const query="INSERT INTO user(Name,Email,Password) VALUES(?);"
+
+    db.query(query,[values],(err,data)=>{
+        if(err){
+            res.send(err);
+        }else{
+            res.send("Register Success!")
+        }
+        
+    })
 }
+
 export const loginUser=(req,res)=>{
     res.send("Login");
 }
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import mysql from 'mysql'
 import cors from 'cors'
-import connectDB from './configs/db.js';
 import userRoutes from './routes/userRoutes.js'
 
 const app=express();
@@ -9,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/user",userRoutes)
 
-const db=connectDB();
+
 
 
 app.listen(5000,()=>{
