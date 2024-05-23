440be29ab6a4a5f140af9ef72eab80ca804e7d85
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2024-05-24 01:55:28 +0530

    creating ui

--- frontend/src/register.jsx
+++ frontend/src/register.jsx
@@ -1,8 +1,33 @@
-import 'react'
+import React from "react";
+import './css/register.css';
+import logo from './assets/logo.png';
+import { useNavigate } from "react-router-dom";
 
 function Register(){
+    const navigate = useNavigate();
     return(
         <>
+
+            <header>
+                <nav className="navbar">
+                    <span className="hamburger-btn material-symbols-rounded">menu</span>
+                    <a href="#" className="logo" onClick={()=> navigate('/')}>
+                        <img src={logo} alt="logo"></img>
+                        <h2><b><span>Travelo</span></b></h2>
+                    </a>
+                                    
+                    {/* <ul className="links">
+                        <span className="close-btn material-symbols-rounded">close</span>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Hotel</a></li>
+                        <li><a href="#">Place</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Contact us</a></li>
+                    </ul> */}
+               
+                </nav> 
+            </header>  
+            
             <div className="wrapper1">
                 <form action=''>
                     <h1>Register</h1>
@@ -18,14 +43,14 @@ function Register(){
                     <input type='password' placeholder='Password' required/>
                         <i className="bx bxs-lock-alt"></i>
                     </div>
-                    <div className="terms">
+                    {/* <div className="terms">
                         <label><input type=''>I agree to </input></label>
                         <a href="#" className="option">Terms & Conditions</a>
-                    </div>
-                    <button type='submit' className="btn">Register</button>
+                    </div> */}
+                    <button type='submit' className="btn" >Register</button>
                     
                     <div className="login-link"></div>
-                    <p>Already have an account?  <a href='#'>Login</a></p>
+                    <p>Already have an account?  <a href='#' onClick={()=> navigate('/login')}>Login</a></p>
                 </form>
             </div>
         </>
