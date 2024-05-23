4eb3b42d135db1db6bfec500407b69eaec032e2f
Sehara Kulasinghe <92316257+sheh00@users.noreply.github.com>
2024-05-23 17:12:36 +0530

    login

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -3,7 +3,24 @@ import 'react'
 function Login(){
     return(
         <>
-        
+            <div class="wrapper">
+                <form action=''>
+                    <h1>Login</h1>
+                    <div class = "input-box">
+                        <input type='text' placeholder='Username' required/>
+                        <i class="bx bxs-user"></i>
+                    </div>
+                    <div class="input-box">
+                    <input type='password' placeholder='Password' required/>
+                        <i class="bx bxs-lock-alt"></i>
+                    </div>
+
+                    <button type='submit' class="btn">Login</button>
+                    
+                    <div class="register-link"></div>
+                    <p>Don't have account?  <a href='#'>Register</a></p>
+                </form>
+            </div>
         </>
     );
 }
