c1dfa0c01283adbaf67e4db301ee4b564778cc2d
Sehara Kulasinghe <92316257+sheh00@users.noreply.github.com>
2024-05-23 18:15:05 +0530

    login css

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -7,14 +7,16 @@ function Login(){
                 <form action=''>
                     <h1>Login</h1>
                     <div class = "input-box">
-                        <input type='text' placeholder='Username' required/>
+                        <input type='text' placeholder='Email' required/>
                         <i class="bx bxs-user"></i>
                     </div>
                     <div class="input-box">
                     <input type='password' placeholder='Password' required/>
                         <i class="bx bxs-lock-alt"></i>
                     </div>
-
+                    <div class="remember">
+                        <label><input type='chechbox'>Remember me</input></label>
+                    </div>
                     <button type='submit' class="btn">Login</button>
                     
                     <div class="register-link"></div>
