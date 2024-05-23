e37a3015157e24ea2d3feb1f744a02516bc77d58
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2024-05-24 01:30:26 +0530

    add the logo to login page also navigate to home page

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import './css/login.css';
+import logo from './assets/logo.png';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
     const navigate = useNavigate();
     
     return(
-        <>      
+        <>  
+            <header>
+                <nav className="navbar">
+                    <span className="hamburger-btn material-symbols-rounded">menu</span>
+                    <a href="#" className="logo" onClick={()=> navigate('/')}>
+                        <img src={logo} alt="logo"></img>
+                        <h2><b><span>Travelo</span></b></h2>
+                    </a>
+{/*                     
+                    <ul className="links">
+                        <span className="close-btn material-symbols-rounded">close</span>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Hotel</a></li>
+                        <li><a href="#">Place</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Contact us</a></li>
+                    </ul>
+                */}
+                </nav> 
+            </header>    
             <div className='wrapper'>
                 <form action=''>
                     <h1>Login</h1>
