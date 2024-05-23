9b8141c7046e4ca017496eb3e14f3efa4a28414f
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2024-05-24 01:07:09 +0530

    there was a error in input checkbox remember me

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,9 +1,13 @@
-import 'react'
+import React from 'react';
+import './css/login.css';
+import { useNavigate } from 'react-router-dom';
 
-function Login(){
+function Login() {
+    const navigate = useNavigate();
+    
     return(
-        <>
-            <div className="wrapper">
+        <>      
+            <div className='wrapper'>
                 <form action=''>
                     <h1>Login</h1>
                     <div className = "input-box">
@@ -14,17 +18,20 @@ function Login(){
                     <input type='password' placeholder='Password' required/>
                         <i className="bx bxs-lock-alt"></i>
                     </div>
-                    <div className="remember">
-                        <label><input type='chechbox'>Remember me</input></label>
-                    </div>
-                    <button type='submit' className="btn">Login</button>
                     
+                    <div className="remember">
+                        <label>
+                            <input type="checkbox" /> Remember me
+                        </label>
+                    </div>
+
+                    <button type='submit' className="btn">Login</button>
                     <div className="register-link"></div>
-                    <p>Don't have account?  <a href='#'>Register</a></p>
+                    <p>Don't have account?  <a href='#'>Register</a></p> 
                 </form>
             </div>
         </>
     );
 }
 
-export default Login
+export default Login;
