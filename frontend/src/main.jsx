98b0b729694143aadf54eb972255560de87c4fc7
Sehara Kulasinghe <92316257+sheh00@users.noreply.github.com>
2024-05-25 21:28:01 +0530

    aboutus

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home.jsx';
 import Login from './login.jsx'; 
 import Register from './register.jsx';
-
+import Aboutus from './aboutus.jsx';
+import Contactus from './contactus.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
@@ -12,6 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/" element={<Home />} /> 
       <Route path="/login" element={<Login />} /> 
       <Route path='/register' element={<Register/>}/>
+      <Route path='/aboutus' element={<Aboutus/>}/>
+      <Route path='/aboutus' element={<Aboutus/>}/>
     </Routes> 
   </Router>
 );
