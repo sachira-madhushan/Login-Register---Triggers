e6a4cb1c57e741b97544f7b7ec41f514fa33c0be
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2024-05-26 18:49:39 +0530

    login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Home from './pages/home';
+import Login from './pages/login';
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </Router>,
   document.getElementById('root')
